46699b0eb6f5d0b6bfe6bbb1c9b9129a68f5ec19
v <adnii@mail.ru>
2022-01-08 22:22:28 +0300

    1st note

---
+++ M_00/ex00/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cctype>
+
+int main(int ac, char **ag)
+{
+	if (ac == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	for (int i = 1; i < ac; i++)
+	{
+		for (int j = 0; ag[i][j]; j++)
+			std::cout << (char ) std::toupper(ag[i][j]);
+		std::cout << " ";
+	}
+	std::cout << std::endl;
+	return 0;
+}
